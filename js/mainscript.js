8b5ac3c3cd18acc29972070c64caaa0a00f6c30e
Ranuka Perera <ranukap@gmail.com>
2015-08-25 02:58:11 +0530

    Used jquery. Moved to MVC model. Used bower for jquery.

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,65 +1,73 @@
-var cats_json = {
-    "cats": [
-        {
-            "name": "Spiffy", "url": "img/unnamed_cat_1.jpg", "desc":"misty Cat", "clicks": 0
+$(function() {
+    var model = {
+        "init": function () {
+            this.cats = [
+                {"name": "Spiffy", "url": "img/unnamed_cat_1.jpg", "desc": "misty Cat", "clicks": 0},
+                {"name": "Pludgy", "url": "img/unnamed_cat_2.jpg", "desc": "Big cat", "clicks": 0},
+                {"name": "Biffy", "url": "img/Cute-Cat-Wallpaper-HD.jpg", "desc": "Big cat", "clicks": 0},
+                {"name": "Kataroma", "url": "img/Cute-Cats-cats-33440930-1280-800.jpg", "desc": "Big cat", "clicks": 0},
+                {"name": "Biffyyii", "url": "img/cute_cat_10_hd_backgrounds.jpg", "desc": "Big cat", "clicks": 0}
+            ]
         },
-        {
-            "name": "Springgy", "url": "img/unnamed_cat_2.jpg", "desc":"Big cat", "clicks": 0
-        },
-        {
-            "name": "Biffy", "url": "img/Cute-Cat-Wallpaper-HD.jpg", "desc":"Big cat", "clicks": 0
-        },
-        {
-            "name": "Springgy", "url": "img/Cute-Cats-cats-33440930-1280-800.jpg", "desc":"Big cat", "clicks": 0
-        },
-        {
-            "name": "Springgy", "url": "img/cute_cat_10_hd_backgrounds.jpg", "desc":"Big cat", "clicks": 0
+        "incrementClick": function (catIndex) {
+            this.cats[catIndex].clicks += 1;
         }
-    ]
-};
 
-var cats = cats_json.cats;
-var currentCat = 0;
-var score_elm = document.getElementsByClassName('score')[0];
+    };
 
-var catList_html = '';
-cats.forEach(function (cat, index) {
-    catList_html += getCatHTML(cat.name, cat.url, cat.desc)[0];
+    var octo = {
+        "getCurrentCat": function () {
+            return model.cats[this.currentCatIndex];
+        },
+        "init": function () {
+            this.currentCatIndex = 0;
+            model.init();
+            view.init();
+        },
+        "catClicked": function () {
+            model.incrementClick(this.currentCatIndex);
+            view.showClickCount();
+        },
+        "getCatList": function () {
+            return model.cats;
+        },
+        "setCurrentCat": function (index) {
+            this.currentCatIndex = index;
+            view.setMainCat();
+        }
+    };
+
+    var view = {
+        "showClickCount": function () {
+            var currentCat = octo.getCurrentCat();
+            $('.score').text('Clicks: ' + currentCat.clicks);
+
+        },
+        "setMainCat": function () {
+            var currentCat = octo.getCurrentCat();
+            var mainCatHTML = '<img src="' + currentCat.url + '" alt="' + currentCat.desc + '" class="image"><span>' + currentCat.name + '</span>';
+            $('.dynamic-cat').html(mainCatHTML);
+            view.showClickCount();
+        },
+        "init": function () {
+            var cats = octo.getCatList();
+            var catList_html = '';
+            cats.forEach(function (cat, index) {
+                catList_html += '<li class="cat-li-item"><span>' + cat.name + '</span><img src="' + cat.url + '" alt="' + cat.desc + '" class="thumb"></li>';
+            });
+            $('.cat-list').html(catList_html);
+            $('.cat-li-item').each(function (index) {
+                $(this).click( function () {
+                    octo.setCurrentCat(index);
+                })
+            });
+            view.setMainCat();
+            $('.cat-info').click(function () {
+                octo.catClicked();
+            });
+
+        }
+    };
+
+    octo.init();
 });
-addToCatList(catList_html);
-
-var catList = document.getElementsByClassName('cat-li-item');
-for (var index = 0; index < catList.length; index++) {
-    var cat = cats[index];
-    catList[index].addEventListener('click', (function(cat, index) {
-        return function () {
-            var mainCatHTML = getCatHTML(cat.name, cat.url, cat.desc)[1];
-            document.getElementsByClassName('dynamic-cat')[0].innerHTML = mainCatHTML;
-            currentCat = index;
-            updateScore();
-        }
-    })(cat, index), false);
-}
-
-document.getElementsByClassName('dynamic-cat')[0].addEventListener('click', function () {
-    var cat = cats[currentCat];
-    cat.clicks += 1;
-    updateScore();
-}, false);
-
-function getCatHTML(cat_name, cat_url, alt_text) {
-
-    var listItem = '<li class="cat-li-item"><span>' + cat_name + '</span><img src="' + cat_url + '" alt="' + alt_text + '" class="thumb"></li>';
-    var mainCatHTML = '<img src="'+cat_url+'" alt="'+alt_text+'" class="image"><span>'+cat_name+'</span>';
-    return [listItem, mainCatHTML];
-
-}
-function addToCatList(html_code) {
-    var catList = document.getElementsByClassName('cat-list')[0];
-    catList.innerHTML = html_code;
-}
-
-function updateScore() {
-    var score = cats[currentCat].clicks;
-    score_elm.textContent = 'Clicks: ' + score;
-}
